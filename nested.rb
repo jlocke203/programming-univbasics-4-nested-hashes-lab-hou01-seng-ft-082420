0d5a082bdf5bb67d3a6089e51211a09b0f990f58
Jaden Locke <jadenlocke1113@gmail.com>
2020-08-16 19:19:41 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -138,6 +138,7 @@ def adding_to_dennis
         :languages => "Assembly"
       }
     }
-    programmer_hash[:dennis_ritchie][:languages] = 
+    programmer_hash[:dennis_ritchie][:languages] = "Assembly"
+    programmer_hash
 
 end
